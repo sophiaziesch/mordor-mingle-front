1433101870156535a1cf2467fe354bf92ac3e980
Sophia Ziesch <s.ziesch@posteo.de>
2023-08-11 10:11:47 +0200

    final commit

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,23 +1,27 @@
 import Comment from "../components/Comment";
 import NewComment from "./NewComment";
 
-const Comments = ({ comments, setComments }) => {
-  return (
-    <div className="comments">
-      <h3>Comments</h3>
-      {comments.map((comment) => {
-        return (
-          <div key={comment._id}>
-            <h4>
-              {" "}
-              <Comment comment={comment} />
-            </h4>
-          </div>
-        );
-      })}
-      <NewComment comments={comments} setComments={setComments} />
-    </div>
-  );
+const Comments = ({ comments, setComments, fetchOneEvent }) => {
+	return (
+		<div className="comments">
+			<h3>Comments</h3>
+			{comments.map((comment) => {
+				return (
+					<div key={comment._id}>
+						<h4>
+							{" "}
+							<Comment comment={comment} />
+						</h4>
+					</div>
+				);
+			})}
+			<NewComment
+				comments={comments}
+				setComments={setComments}
+				fetchOneEvent={fetchOneEvent}
+			/>
+		</div>
+	);
 };
 
 export default Comments;
